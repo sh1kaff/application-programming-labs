9277dd987351c71786fbc192b9d422033c81c30d
Big Brother <example@mail.com>
2024-09-07 20:23:29 +0400

    Create function to find common codes in file

--- lab1/main.py
+++ lab1/main.py
@@ -3,6 +3,23 @@ import re
 
 number_regex = r"(?:(?:\+?[7])|(?:\b[8])) ?(\d{3}) ?\d{3}-?\d{2}-?\d{2}"
 
+def get_common_codes(filename):
+    matches = dict()
+
+    with open(filename, "r", encoding="utf-8") as file:
+        c = re.compile(number_regex)
+
+        while ( line := file.readline() ):
+            if line.startswith("Номер"):
+                code = c.search(line).group(1)
+
+                if code in matches: matches[code] += 1
+                else: matches[code] = 1
+
+    max_match = max(matches.values())
+
+    for code, match in matches.items():
+        if match == max_match: yield code
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
