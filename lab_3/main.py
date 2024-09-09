208732d873bfc8d12f780ed43c3384609b8c79a1
Big Brother <aykibugz@yandex.ru>
2024-09-09 17:16:50 +0400

    Divided the function

--- lab_3/main.py
+++ lab_3/main.py
@@ -1,15 +1,26 @@
 # https://habr.com/ru/companies/otus/articles/558426/
 # https://tproger.ru/translations/opencv-python-guide
+# https://ru.wikipedia.org/wiki/Гистограмма_(фотография)
+# https://www.geeksforgeeks.org/python-opencv-cv2-calchist-method/
+# https://docs.opencv.org/3.4/d8/dbc/tutorial_histogram_calculation.html
+
+import argparse
 
 import cv2
 from matplotlib import pyplot as plt
 
+def _parse_arguments():
+    parser = argparse.ArgumentParser(
+        prog="main.py",
+        description="...",
+        epilog="o:" 
+    )
+    parser.add_argument("img_main", type=str, help="...")
+    parser.add_argument("img_overlay", type=str, help="....")
+    
+    return parser.parse_args()
 
-def main():
-    img = cv2.imread("images/1.jpg")
-    print(img.shape)  # sizes in h,w,c
-    print(img.size)  # size in bytes
-
+def show_hist(img):
     colors = ("blue", "green", "red", "black")
     legend = (*map(lambda s: s.capitalize() + " channel", colors[:-1]), "Summary")
 
@@ -30,6 +41,21 @@ def main():
 
     plt.title("Image Histogram")
     plt.show()
+
+
+def main():
+    args = _parse_arguments()
+    
+    img1 = cv2.imread(args.img_main)
+    img2 = cv2.imread(args.img_overlay)
+
+    print(img1.shape)  # sizes in h,w,c
+    print(img1.size)  # size in bytes
+
+    show_hist(img1)
+    show_hist(img2)
+
+    
 
 
 
