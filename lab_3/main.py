28fb108f2f27c41ce7d52d46bfc63a368153f7a3
Big Brother <aykibugz@yandex.ru>
2024-09-09 17:04:54 +0400

    Add histogram show

--- lab_3/main.py
+++ lab_3/main.py
@@ -2,14 +2,37 @@
 # https://tproger.ru/translations/opencv-python-guide
 
 import cv2
-from sys import getsizeof
-
-img = cv2.imread("images/1.jpg")
-print(img.size)
+from matplotlib import pyplot as plt
 
 
-print(type(img))
+def main():
+    img = cv2.imread("images/1.jpg")
+    print(img.shape)  # sizes in h,w,c
+    print(img.size)  # size in bytes
+
+    colors = ("blue", "green", "red", "black")
+    legend = (*map(lambda s: s.capitalize() + " channel", colors[:-1]), "Summary")
+
+    hist = None
+    for channel, color in zip( (0, 1, 2), colors[:-1] ):
+        current_hist = cv2.calcHist([img], [channel], None, [256], [0, 256])
+        if hist is None:
+            hist = current_hist
+        else:
+            hist += current_hist
+        plt.plot(current_hist, color=color)
+
+    plt.plot(hist, color=colors[-1])
+    plt.ylabel("Pixels")
+    plt.xlabel("Brightness")
+
+    plt.legend(legend)
+
+    plt.title("Image Histogram")
+    plt.show()
 
 
-# cv2.imshow("cat", img)
-# cv2.waitKey(0)
+
+if __name__ == "__main__":
+    main()
+
