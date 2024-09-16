dcef9d12a89490f377b68e0fb0bac6550f8141c0
Big Brother <aykibugz@yandex.ru>
2024-09-16 11:08:18 +0400

    Beautify image.py

--- lab_3/image.py
+++ lab_3/image.py
@@ -3,13 +3,17 @@ import cv2
 def get_hist(img, channel):
     return cv2.calcHist([img], [channel], None, [256], [0, 256])
 
-def overlay_images(img_main, img_overlay, trn: int):
+def overlay_images(img_main, img_overlay, trn: int, force: bool = True):
     if trn > 100 or trn < 0:
         raise ValueError("fignya")
 
     trn_result = 1 - trn / 100
 
-    img_overlay_resized = cv2.resize(img_overlay, (img_main.shape[1], img_main.shape[0]))
-    img_result = cv2.addWeighted(img_main, 1 - trn_result, img_overlay_resized, trn_result, 0)
+    if not force and (img_main.shape[:2] != img_overlay.shape[:2]):
+        raise ValueError("Sizes don't match")
+    elif force:
+        img_overlay = cv2.resize(img_overlay, (img_main.shape[1], img_main.shape[0]))
+        
+    img_result = cv2.addWeighted(img_main, 1 - trn_result, img_overlay, trn_result, 0)
 
     return img_result
