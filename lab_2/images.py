8a1952a3a80e2fd2e71dd9fcae19b0223549ff12
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2024-09-23 13:57:19 +0400

    Update images.py
    Edit some functionality

--- lab_2/images.py
+++ lab_2/images.py
@@ -1,21 +1,27 @@
 from icrawler.builtin import GoogleImageCrawler
 
 base_dir = "images"
+little_dir = "images"
 
-def download_images(keyword: str, num: int, dir: str = "images") -> str:
+def download_images(keyword: str, num: int, dir: str = little_dir) -> str:
     """
     Function for downloading images from Google using icrawler.
 
     Parameters:
     keyword (str): what are you search.
     num (int): number of photos you want download.
-    dir (str): directory to save. Default: "images"
+    dir (str): directory to save. Default: "images/images_{keyword}"
 
     Returns:
     str: path to directory where files save.
     """
 
-    root_dir = f"{base_dir}\\{dir}"
+    if dir.startswith( ("C:", ".") ):
+        root_dir = dir
+    else:
+        root_dir = f"{base_dir}\\{dir}"
+        if dir == little_dir: root_dir += f"_{keyword}"
+        
     google_crawler = GoogleImageCrawler(
         storage={"root_dir": root_dir},
         feeder_threads=1,
