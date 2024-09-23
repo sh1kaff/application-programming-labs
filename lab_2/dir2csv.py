301fe034c5ef293739bce2f3e3dbff1a25799959
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2024-09-23 13:56:56 +0400

    Update dir2csv.py
    Edit some functionality

--- lab_2/dir2csv.py
+++ lab_2/dir2csv.py
@@ -1,23 +1,36 @@
 import csv
 import os
 
-def dir2csv(dir: str, csv_name: str = "some") -> None:
+default_name = "some.csv"
+
+def dir2csv(dir: str, csv_path: str = default_name) -> str:
     """
     Function to convert files in directory to csv file with list of links.
-
+    (Don't create parent dirs if don't exists.)
+    
     Parameters:
     dir (str): path to directory where stored the images. 
-    csv_name (str): name csv-file (without ext.) for new file. Default: "some"
+    csv_path (str): path to csv-file for new file. Default: "some.csv".
     """
-    
-    with open(csv_name + ".csv", "w", newline="") as csvfile:
+
+    if not os.path.split(csv_path)[-1]:
+        csv_path += default_name
+
+    if not csv_path.endswith(".csv"):
+        csv_path += ".csv"
+
+    with open(csv_path, "w", newline="") as csvfile:
+        abs_dir = os.path.abspath(dir)
         names = ("Rel. path", "Abs. path")
 
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(names)
 
-        for dirfile in os.listdir(dir):
-            abs_path = os.path.abspath(dirfile)
-            rel_path = os.path.relpath(abs_path, start=__file__)
+        for file in os.listdir(dir):
+            abs_path = os.path.join(abs_dir, file)
+            rel_path = os.path.relpath(abs_path, start=csv_path)
+            print(rel_path, abs_path)
 
             csv_writer.writerow((rel_path, abs_path))
+    
+    return csv_path
