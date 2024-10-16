e8d41b3bb69a77a4cc5b5f31ea246b53a3a032da
Big Brother <aykibugz@yandex.ru>
2024-10-16 17:16:11 +0400

    Add column naming

--- lab_4/main.py
+++ lab_4/main.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 def main():
-    df = pd.read_csv(".csv")
-    print(df.head)
+    df = pd.read_csv("data.csv", names=list("sdfgh"))
+    print(df)
 
 if __name__ == "__main__":
     main()
