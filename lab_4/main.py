d6c81b97a0eaa140d89cab28b29fb03c26ec2294
Big Brother <aykibugz@yandex.ru>
2024-10-16 17:49:09 +0400

    Add getting images sizes

--- lab_4/main.py
+++ lab_4/main.py
@@ -1,8 +1,23 @@
 import pandas as pd
+from cv2 import imread
 
 def main():
-    df = pd.read_csv("data.csv", names=list("sdfgh"))
-    print(df)
+    df = pd.read_csv("putin.csv", names=["RP", "AP", "H", "W", "CH"], skiprows=1)
+    
+    # for row in df:
+    #     print(row)
+    #     break
+
+    for idx in range( len(df) ):
+        path = df.at[idx, "AP"]
+        img = imread(path)
+        sizes = img.size if img else None
+        print(sizes)
+
+
+    print(len(df))
+
+    # print(df.at[0, "RP"])
 
 if __name__ == "__main__":
     main()
